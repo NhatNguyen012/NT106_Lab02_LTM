538fbe4675c53f130847009aa460cce9fc072bfe
TrThuyTien <tranthithuytien0615@gmail.com>
2024-10-14 01:57:46 +0700

    Designed interface

--- Lab2/Properties/Resources.Designer.cs
+++ Lab2/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Lab2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap exit_32px {
+            get {
+                object obj = ResourceManager.GetObject("exit_32px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap left {
             get {
                 object obj = ResourceManager.GetObject("left", resourceCulture);
