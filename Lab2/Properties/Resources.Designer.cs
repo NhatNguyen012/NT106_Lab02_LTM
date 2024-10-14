7f712c30619f467ff50b0b480b8280ec6c4bb260
TrThuyTien <tranthithuytien0615@gmail.com>
2024-10-14 16:07:19 +0700

    Fixed BT3

--- Lab2/Properties/Resources.Designer.cs
+++ Lab2/Properties/Resources.Designer.cs
@@ -159,5 +159,15 @@ namespace Lab2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap trash {
+            get {
+                object obj = ResourceManager.GetObject("trash", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
