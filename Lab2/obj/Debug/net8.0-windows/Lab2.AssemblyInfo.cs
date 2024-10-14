3e129054c1369110bddbe246b3feae70b92d3fe1
NNguyenK18 <nhatnguyen211278@gmail.com>
2024-10-14 16:46:24 +0700

    Edit Logic BT1

--- Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7811f653050f4d30aeaaf42112882f6fc26db950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b54890573fd0b77e396280d7c366145fdee9bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
